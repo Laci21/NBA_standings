f9a3138664fc2157370e74a918c422e83af878f6
lac21 <laszlonagy93@gmail.com>
2016-04-17 14:24:48 +0200

    Presenter added.

---
+++ app/src/main/java/main/nba_standings/ui/Presenter.java
@@ -0,0 +1,14 @@
+package main.nba_standings.ui;
+
+public abstract class Presenter<S> {
+    protected S screen;
+
+    public void attachScreen(S screen) {
+        this.screen = screen;
+    }
+
+    public void detachScreen() {
+        this.screen = null;
+    }
+}
+
