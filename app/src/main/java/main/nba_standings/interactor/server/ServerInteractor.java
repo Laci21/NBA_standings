fca9fa9ac3d188f222ec4ab191a3c7cc8eaecbb0
lac21 <laszlonagy93@gmail.com>
2016-04-18 17:18:18 +0200

    ORM for server added.

---
+++ app/src/main/java/main/nba_standings/interactor/server/ServerInteractor.java
@@ -0,0 +1,34 @@
+package main.nba_standings.interactor.server;
+
+import java.util.List;
+
+import main.nba_standings.model.TeamDataTable;
+
+public class ServerInteractor {
+    public ServerInteractor() {
+    }
+
+    public TeamDataTable findTeam(String teamName) {
+        List<TeamDataTable> teamDataTableList = TeamDataTable.find(TeamDataTable.class, "team_name = ?", teamName);
+        if (teamDataTableList != null && !teamDataTableList.isEmpty()) {
+            return teamDataTableList.get(0);
+        }
+        return null;
+    }
+
+    public void saveTeam(TeamDataTable teamToSave) {
+        teamToSave.save();
+    }
+
+    public void updateTeam(TeamDataTable teamToUpdate) {
+        deleteTeam(teamToUpdate.getTeamName());
+        teamToUpdate.save();
+    }
+
+    public void deleteTeam(String teamName) {
+        TeamDataTable teamDataTable = findTeam(teamName);
+        if(teamDataTable != null){
+            teamDataTable.delete();
+        }
+    }
+}
